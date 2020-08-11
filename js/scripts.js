1e11b28d9fe21733ec76879089331f38728756d5
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-08-11 16:01:17 -0700

    spelling error fixed, cards to be duplicated for more working flash card definitions.
    Co-authored-by: Sean Wilson <paintballsean@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
-$(document).ready(function() {
-  $(".toggleOne").click(function() {
-    $("#card-word").fadeToggle();
-    $("#card-definition").fadeToggle();
+$(document).ready(function () {
+  $(".toggleOne").click(function () {
+    $("#card-word").toggle();
+    $("#card-definition").toggle();
   });
-  $(".toggleTwo").click(function() {
-    $("#card-word-two").fadeToggle();
-    $("#card-definition-two").fadeToggle();
+  $(".toggleTwo").click(function () {
+    $("#card-word-two").toggle();
+    $("#card-definition-two").toggle();
   });
 });
-  
+
 /*$('.card-body > p > span').click(function() {
     $(this) .parent().toggleClass('card-body');
     return false; */
